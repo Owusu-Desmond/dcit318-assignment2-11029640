2a885663f7691eb46549a652e086799839dde0da
Owusu-Desmond <desmond0406114009@gmail.com>
2025-08-09 11:11:37 +0000

    Abstract Classes & Methods

--- Program.cs
+++ Program.cs
@@ -26,6 +26,43 @@ namespace OPPExamples
         }
     }
 
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -38,6 +75,15 @@ namespace OPPExamples
             genericAnimal.MakeSound();
             dog.MakeSound();
             cat.MakeSound();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Abstract Classes & Methods");
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
         }
     }
 }
